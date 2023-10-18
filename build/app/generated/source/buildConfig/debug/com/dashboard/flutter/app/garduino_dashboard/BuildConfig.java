6a4de41fc918b958f7453c509ab82a37ad51df55
Argha Das <hdark6336@gmail.com>
2023-10-18 22:46:15 +0530

    first commit

---
+++ build/app/generated/source/buildConfig/debug/com/dashboard/flutter/app/garduino_dashboard/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dashboard.flutter.app.garduino_dashboard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dashboard.flutter.app.garduino_dashboard";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
